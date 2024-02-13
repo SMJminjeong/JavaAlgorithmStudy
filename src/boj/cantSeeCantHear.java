b0447a0c29aa596be00a327c321afc3dcccca42b
SMJminjeong <sdwp0920@gmail.com>
2024-02-13 20:10:38 +0900

    :triangular_flag_on_post: add 1764 듣보잡

---
+++ src/boj/cantSeeCantHear.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class cantSeeCantHear {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+
+        Set<String> unHeardSet = new HashSet<>();
+        for(int i = 0; i < N; i++) {
+            unHeardSet.add(br.readLine());
+        }
+
+        ArrayList<String> interSectionArr = new ArrayList<>();
+        for(int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if(unHeardSet.contains(name)) {
+                interSectionArr.add(name);
+            }
+        }
+
+        Collections.sort(interSectionArr);
+
+        System.out.println(interSectionArr.size());
+        for (String name : interSectionArr) {
+            System.out.println(name);
+        }
+
+    }
+}
