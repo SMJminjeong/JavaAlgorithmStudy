ac18e79381074315bed6fe241e7bf5080983c5f2
SMJminjeong <sdwp0920@gmail.com>
2024-02-13 23:42:54 +0900

    :triangular_flag_on_post: add 9324 염색체

---
+++ src/boj/chromosome.java
@@ -0,0 +1,35 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class chromosome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            String s = br.readLine();
+            boolean check =false;
+            int a= s.indexOf("A");
+            int f= s.indexOf("F");
+            int c = s.indexOf("C");
+            int aLast= s.lastIndexOf("A");
+            int fLast= s.lastIndexOf("F");
+            if(s.charAt(s.length()-1)=='A' ||s.charAt(s.length()-1)=='F'||s.charAt(s.length()-1)=='C'){
+                if(a<f && f<c){
+                    if(aLast==f-1 && fLast==c-1){
+                        check=true;
+                    }
+                }
+            }
+            if(check){
+                System.out.println("Infected!");
+            }
+            else{
+                System.out.println("Good");
+            }
+        }
+    }
+
+}
---
+++ src/boj/염색체2.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 염색체2 {
+
+    /**
+     * ^ : 정규표현식 시작
+     * $ : 정규표현식 끝
+     * [A-F] : A,B,C,D,E,F
+     * ? : 0번 또는 1번
+     * + : 그 전 문자가 1개 이상
+     */
+    private static final String REGEX = "^[A-F]?A+F+C+[A-F]?$";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (T-->0) {
+            sb.append(br.readLine().matches(REGEX)?"Infected!\n":"Good\n");
+        }
+        System.out.print(sb);
+    }
+}
