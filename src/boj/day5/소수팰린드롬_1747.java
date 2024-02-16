18e82a4f44035ccb763b18f7b9dd0c55fb5752f2
SMJminjeong <sdwp0920@gmail.com>
2024-02-16 23:02:20 +0900

    :triangular_flag_on_post: 소수&팰린드롬_1747

---
+++ src/boj/day5/소수팰린드롬_1747.java
@@ -0,0 +1,61 @@
+package boj.day5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 소수팰린드롬_1747 {
+
+    private static boolean[] primeArr = new boolean[1004001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        checkPrimeNumber();
+
+        int result = 0;
+
+        for (int i = N; i <= 1004000; i++) {
+            if (i < 10 && primeArr[i]) {
+                result = i;
+                break;
+            } else {
+                if (checkPelin(Integer.toString(i)) && primeArr[i]) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+    static void checkPrimeNumber() {
+        for (int i = 2; i<=1004000; i++) {
+            primeArr[i] = true;
+        }
+
+        int num = (int)Math.sqrt(1004000);
+
+        for (int i = 2; i<=num; i++) {
+            if(primeArr[i]) {
+                for (int j = i; i*j <= 1004000; j++) {
+                    primeArr[i*j] = false;
+                }
+            }
+        }
+    }
+
+    static boolean checkPelin(String num) {
+        int start = 0;
+        int end = num.length()-1;
+
+        while (start <= end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
